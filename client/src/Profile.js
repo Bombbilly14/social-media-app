16fcd96f5952fea3db5be64d1b0c67efae5895c7
Hoang Tran <jashoang2583@gmail.com>
2023-01-23 15:57:19 -0800

    test

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Profile = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
-    <div>Profile</div>
-  )
+    <div>
+      {users.map(user => (
+        <div key={user.id}>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>{user.avatar}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
