6892642bf8da2307bf293439a0adb0d0906e26f4
Hoang Tran <jashoang2583@gmail.com>
2023-01-25 12:13:01 -0800

    make navbar more responsive

---
+++ client/src/navData.js
@@ -0,0 +1,50 @@
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import MenuBookIcon from '@mui/icons-material/MenuBook'; 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+
+import SendIcon from '@mui/icons-material/Send';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+export const navData = [
+    {
+        id: 0,
+        icon: <HomeIcon/>,
+        text: "Home",
+        link: "/"
+    },
+    {
+        id: 1,
+        icon: <AccountCircleIcon/>,
+        text: "Profile",
+        link: "/profile"
+    },
+    {
+        id: 2,
+        icon: <SendIcon/>,
+        text: "Messages",
+        link: "/messages"
+    },
+    {
+        id: 3,
+        icon: <NotificationsIcon/>,
+        text: "Notifications",
+        link: "/notifications"
+    },
+
+    {
+        id: 5,
+        icon: <HowToRegIcon/>,
+        text: "Signup/Login",
+        link: "/signin"
+    },
+
+    // {
+    //     id: 6,
+    //     icon: <VerifiedUserIcon/>,
+    //     text: "login",
+    //     link: "/login"
+    // }
+]
