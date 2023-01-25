8c62aa34686acb3c156aef5805d3c22e4cf83f93
William German <william.j.german2@gmail.com>
2023-01-25 09:42:46 -0800

    form styling

--- client/src/RegistrationForm.js
+++ client/src/RegistrationForm.js
@@ -1,11 +1,24 @@
 import React, {useState, useEffect} from 'react'
 
+
 function RegistrationForm({setUser}) {
   const [name, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
 
   const createAccount = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match');
+      return
+    }
+    //@@test
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      setMessage('Password must be at least 8 characters long, contain at least one letter and one number');
+      return
+    }
     fetch('/users', {
     method: "POST",
     headers: {
@@ -24,11 +37,11 @@ function RegistrationForm({setUser}) {
 }
   return (
     <>
+    {message && <p>{message}</p>}
     <h2>Sign Up!</h2>
     <form onSubmit={createAccount}>
         <label htmlFor="name">name</label>
         <input
-        // type="text"
         id="name"
         value={name}
         onChange={ e => setUsername(e.target.value)}/>
@@ -40,6 +53,15 @@ function RegistrationForm({setUser}) {
         value ={password}
         onChange={ (e) => setPassword(e.target.value)}
          />
+         <br />
+        <label htmlFor="confirm-password">Confirm Password:</label>
+        <input
+        type="password"
+        id="confirm-password"
+        value={confirmPassword}
+        onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+          <br />
         <input type= "submit" value="create account" />
     </form>
     </>
