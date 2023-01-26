49e294475f45a27e3aee3e4c5d21f4e9eb6cdded
Hoang Tran <jashoang2583@gmail.com>
2023-01-26 15:39:11 -0800

    still needs work

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -57,19 +57,12 @@
 
 import React from 'react'
 import { NavLink } from "react-router-dom"
-import {useHistory} from 'react'
 import { navData } from "./navData.js";
 import styles from "./styles/navbar.module.css"
 import { useState } from "react";
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-
-import { Link } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import Ventbox from './Ventbox'
-import Popup from 'reactjs-popup'
 import CreateIcon from '@mui/icons-material/Create';
 
 export default function NavBar() {
@@ -96,17 +89,8 @@ export default function NavBar() {
           })}
               <NavLink key={'ventbox'} className={styles.sideitem} to="/vent">
                 <CreateIcon />
-                <Ventbox style={styles.linkText} />
+                <Ventbox to="/vent" style={styles.linkText} />
               </NavLink>
-                {/* <Link className={styles.sideitem}href="https://www.facebook.com/" target="_blank">
-              <FacebookIcon sx={{ color: "#3b5998" }}/>
-            </Link>
-            <Link className={styles.sideitem}href="https://www.youtube.com/watch?v=Whm94pH53yE" target="_blank">
-              <YouTubeIcon sx={{ color: "#FF0000" }}/>
-            </Link>
-            <Link className={styles.sideitem}href="https://www.instagram.com" target="_blank">
-              <InstagramIcon color='secondary'/>
-            </Link> */}
       </div>
     )
   }
