bb97244d6375648d7b98c681a7a265b7dfc5093a
Ryan See <ryanjosephsee@gmail.com>
2023-01-27 11:23:22 -0800

    faker updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,41 @@ require 'faker'
     puts "🌱 Seeding data..."
     
 
-    u1=User.create(name: "Jason T", bio: "Bio for Jason", email: "ex1@ex1.com", password: "password", avatar: Faker::Avatar.image)
-    u2=User.create(name: "William G", bio: "Bio for William", email: "ex2@ex2.com", password: "password", avatar: Faker::Avatar.image)
-    u3=User.create(name: "Ryan S", bio: "Bio for Ryan", email: "ex3@ex3.com", password: "password", avatar: Faker::Avatar.image)
+    u1=User.create(name: "Jason T", bio: Faker::Hipster.paragraph(sentence_count: 3), email: "ex1@ex1.com", password: "password", avatar: Faker::Avatar.image)
+    u2=User.create(name: "William G", bio: Faker::Hipster.paragraph(sentence_count: 3), email: "ex2@ex2.com", password: "password", avatar: Faker::Avatar.image)
+    u3=User.create(name: "Ryan S", bio: Faker::Hipster.paragraph(sentence_count: 3), email: "ex3@ex3.com", password: "password", avatar: Faker::Avatar.image)
 
+
+    Faker::Config.locale = 'en'
 
     10.times do
-        user = User.create!(name: Faker::Name.name, avatar: Faker::Avatar.image, bio: Faker::Lorem.paragraph, password: Faker::Internet.password, email: Faker::Internet.email, slug: Faker::Internet.slug)
+        user = User.create!(
+            name: Faker::Name.name, 
+            avatar: Faker::Avatar.image, 
+            bio: Faker::Hipster.paragraph, 
+            password: Faker::Internet.password, 
+            email: Faker::Internet.email, 
+            slug: Faker::Internet.slug
+        )
         8.times do
-            post = Post.create!(user_id: User.all.sample.id, likes: Faker::Number.between(from: 0, to: 1000), content: Faker::Lorem.sentence, time_posted: Faker::Time.between(from: 20.days.ago, to: Time.now))
-
-        5.times do
-        Comment.create!(content: Faker::Lorem.sentence, likes: Faker::Number.between(from: 0, to: 1000), post_id: post.id, user_id: User.all.sample.id)
+            post = Post.create!(
+                user_id: User.all.sample.id, 
+                likes: Faker::Number.between(from: 0, to: 1000), 
+                content: Faker::Hipster.sentence, 
+                time_posted: Faker::Time.between(from: 20.days.ago, to: Time.now)
+            )
+            5.times do
+                Comment.create!(
+                    content: Faker::Hipster.sentence, 
+                    likes: Faker::Number.between(from: 0, to: 1000), 
+                    post_id: post.id, 
+                    user_id: User.all.sample.id
+                )
+            end
         end
-        end
-        end
+    end
+    
+    
 
 puts "🌱 Seeding Done!"
 #
