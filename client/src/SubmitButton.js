8cce8f486e8d9562214fbe7335aca4ab7eaf2e6b
Hoang Tran <jashoang2583@gmail.com>
2023-01-29 14:36:02 -0800

    fix react router dom errors

---
+++ client/src/SubmitButton.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// function Button({word}) {
+//   return (
+//     <a className="h-button center" data-text="Hover me" href="#" aria-label={word}>
+//       {word.split('').map((letter, index) => (
+//         <span key={index}>{letter}</span>
+//       ))}
+//     </a>
+//   );
+// }
+
+// export default Button;
