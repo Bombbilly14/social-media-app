017deec4043cd51b7f898b676bfb814e101e1f5f
Hoang Tran <jashoang2583@gmail.com>
2023-01-28 20:54:51 -0800

    clean up code and change login icon

--- client/src/navData.js
+++ client/src/navData.js
@@ -1,16 +1,6 @@
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import MenuBookIcon from '@mui/icons-material/MenuBook'; 
+import HomeIcon from '@mui/icons-material/Home'; 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import HowToRegIcon from '@mui/icons-material/HowToReg';
-import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
-
-import SendIcon from '@mui/icons-material/Send';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import CreateIcon from '@mui/icons-material/Create';
-import Ventbox from './Ventbox'
-import Popup from 'reactjs-popup'
-import {useState} from 'react'
+import LoginIcon from '@mui/icons-material/Login';
 
 
 export const navData = [
@@ -27,31 +17,12 @@ export const navData = [
         text: "Profile",
         link: "/profile"
     },
-    // {
-    //     id: 2,
-    //     icon: <SendIcon/>,
-    //     text: "Messages",
-    //     link: "/messages"
-    // },
-    // {
-    //     id: 3,
-    //     icon: <NotificationsIcon/>,
-    //     text: "Notifications",
-    //     link: "/notifications"
-    // },
-
+ 
     {
         id: 2,
-        icon: <HowToRegIcon/>,
+        icon: <LoginIcon/>,
         text: "Signup/Login",
         link: "/signin"
     },
 
-    // {
-    //     id: 6,
-    //     icon: <CreateIcon/>,
-    //     text: "VentBox",
-    //     component: "Ventbox",
-    //     link: "/vent"
-    // }
 ]
